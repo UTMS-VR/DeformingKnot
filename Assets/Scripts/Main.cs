d82d376299d9d170733d41020c77abb9df7d0844
1123sodium <1997081506124772930na1123@gmail.com>
2020-04-02 03:04:40 +0900

    Add close and delete function

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -27,6 +27,7 @@ public class Main : MonoBehaviour
     void Update()
     {
         controller.Update();
+        Vector3 nowPosition = controller.rightHand.GetPosition();
 
         // Aボタン:曲線を生成
         if (controller.GetButtonDown(OVRInput.RawButton.A))
@@ -34,15 +35,13 @@ public class Main : MonoBehaviour
             curveList.Add(new Curve(false, true, false, false, new List<Vector3>(), Vector3.zero, Quaternion.identity));
         }
 
-        // Aボタン長押し:曲線を描画
-        if (controller.GetButton(OVRInput.RawButton.A))
+        foreach (Curve curve in curveList)
         {
-            foreach (Curve curve in curveList)
+            if (curve.isBeingDrawn)
             {
-                if (curve.isBeingDrawn)
+                // Aボタンを押している間:曲線を描画
+                if (controller.GetButton(OVRInput.RawButton.A))
                 {
-                    Vector3 nowPosition = controller.rightHand.GetPosition();
-
                     if (curve.positions.Count == 0)
                     {
                         curve.positions.Add(nowPosition);
@@ -55,6 +54,29 @@ public class Main : MonoBehaviour
                         curve.MeshUpdate();
                     }
                 }
+                else if (controller.GetButtonUp(OVRInput.RawButton.A))
+                {
+                    curve.isBeingDrawn = false;
+                }
+            }
+
+            // 右手人差し指:曲線を選択
+            if (controller.GetButtonDown(OVRInput.RawButton.RIndexTrigger) && Dist(curve.positions, nowPosition) < collision)
+            {
+                curve.isSelected = !curve.isSelected;
+            }
+
+            // Bボタン:曲線を閉じる
+            if (curve.isSelected && controller.GetButtonDown(OVRInput.RawButton.B))
+            {
+                curve.isClosed = !curve.isClosed;
+                curve.MeshUpdate();
+            }
+
+            // Yボタン:曲線を消去
+            if (curve.isSelected && controller.GetButtonDown(OVRInput.RawButton.Y))
+            {
+                curveList.Remove(curve);
             }
         }
 
@@ -85,6 +107,47 @@ public class Main : MonoBehaviour
             ( OVRInput.RawButton.LIndexTrigger, KeyCode.Q ),
             ( OVRInput.RawButton.LHandTrigger, KeyCode.W )
         });
+
+    private List<Vector3> MapPlus(List<Vector3> positions, Vector3 position)
+    {
+        List<Vector3> newPositions = new List<Vector3>();
+
+        foreach (Vector3 v in positions)
+        {
+            newPositions.Add(v + position);
+        }
+
+        return newPositions;
+    }
+
+    private float Dist(List<Vector3> positions, Vector3 position)
+    {
+        List<Vector3> relPositions = MapPlus(positions, -position);
+
+        float min = relPositions[0].magnitude;
+
+        for (int i = 0; i < relPositions.Count - 1; i++)
+        {
+            if (relPositions[i + 1].magnitude < min)
+            {
+                min = relPositions[i + 1].magnitude;
+            }
+        }
+
+        return min;
+    }
+
+    private List<Vector3> MapRotation(List<Vector3> positions, Quaternion rotation)
+    {
+        List<Vector3> newPositions = new List<Vector3>();
+
+        foreach (Vector3 v in positions)
+        {
+            newPositions.Add(rotation * v);
+        }
+
+        return newPositions;
+    }
 
     public Material MaterialChoice(bool isSelected)
     {
