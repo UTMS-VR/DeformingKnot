bf3902f8e73a262c4038c47a52a891589c00b8e6
1123sodium <1997081506124772930na1123@gmail.com>
2020-04-06 04:40:29 +0900

    Add MyController

---
+++ Assets/Scripts/MyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DebugUtil;
+
+public class MyController
+{
+    public static void SetUp(ref Controller controller)
+    {
+        controller = new Controller(
+            buttonMap: LiteralKeysPlus,
+            rightHandMover: Stick3DMap.OKLSemiIComma,
+            handScale: 0.03f,
+            handSpeed: 0.01f
+        );
+    }
+
+    public static void Update(Controller contoller)
+    {
+        contoller.Update();
+    }
+
+    private static ButtonMap LiteralKeysPlus
+        = new ButtonMap(new List<(OVRInput.RawButton, KeyCode)>{
+            ( OVRInput.RawButton.A, KeyCode.A ),
+            ( OVRInput.RawButton.B, KeyCode.B ),
+            ( OVRInput.RawButton.X, KeyCode.X ),
+            ( OVRInput.RawButton.Y, KeyCode.Y ),
+            ( OVRInput.RawButton.RIndexTrigger, KeyCode.R ),
+            ( OVRInput.RawButton.RHandTrigger, KeyCode.E ),
+            ( OVRInput.RawButton.LIndexTrigger, KeyCode.Q ),
+            ( OVRInput.RawButton.LHandTrigger, KeyCode.W )
+        });
+}
