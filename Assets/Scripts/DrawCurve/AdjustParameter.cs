b7c88722a5ce2dca98405330bfa64786a6826655
1123sodium <1997081506124772930na1123@gmail.com>
2020-06-29 19:12:40 +0900

    Add various Equalize

--- Assets/Scripts/DrawCurve/AdjustParameter.cs
+++ Assets/Scripts/DrawCurve/AdjustParameter.cs
@@ -1,36 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace DrawCurve
 {
     public class AdjustParameter
     {
-        public static void Equalize(ref List<Vector3> positions, float segment, bool closed)
+        public static void EqualizeP(ref List<Vector3> positions, float segment, bool closed)
+        {
+            int division = DivisionNumber(positions, segment, closed);
+            EqualizeD(ref positions, division, closed);
+        }
+
+        public static void EqualizeL(ref List<Vector3> positions, float segment, int division, bool closed)
+        {
+            float arcLength = ArcLength(positions, closed);
+            positions = positions.Select(v => v * segment * division / arcLength).ToList();
+            EqualizeD(ref positions, division, closed);
+        }
+
+        public static void EqualizeD(ref List<Vector3> positions, int division, bool closed)
         {
             int length = positions.Count;
+            float segment = ArcLength(positions, closed) / division;
+
             List<Vector3> newpositions = new List<Vector3>();
             newpositions.Add(positions[0]);
             float remainder = 0.0f;
-            float temporarySegment = ArcLength(positions, closed) / DivisionNumber(positions, segment, closed);
 
             for (int i = 1; i < length; i++)
             {
-                Completion(ref newpositions, positions[i - 1], positions[i], ref remainder, temporarySegment);
+                Completion(ref newpositions, positions[i - 1], positions[i], ref remainder, segment);
             }
 
             if (closed)
             {
-                Completion(ref newpositions, positions[length - 1], positions[0], ref remainder, temporarySegment);
+                Completion(ref newpositions, positions[length - 1], positions[0], ref remainder, segment);
 
-                if (newpositions.Count > DivisionNumber(positions, segment, closed))
+                if (newpositions.Count > division)
                 {
                     newpositions.Remove(newpositions[newpositions.Count - 1]);
                 }
             }
             else
             {
-                if (newpositions.Count < DivisionNumber(positions, segment, closed) + 1)
+                if (newpositions.Count < division + 1)
                 {
                     newpositions.Add(positions[length - 1]);
                 }
