faa35c5345e43b684d49aa99e9221604d0011271
1123sodium <1997081506124772930na1123@gmail.com>
2020-07-07 04:42:56 +0900

    Add Elasticity

---
+++ Assets/Scripts/GradientFlow/Elasticity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elasticity
+{
+
+    private List<Vector3> positions;
+    private int length;
+    private float lr;
+
+    public Elasticity(List<Vector3> positions, float lr)
+    {
+        this.positions = positions;
+        this.length = positions.Count;
+        this.lr = lr;
+    }
+
+    public Vector3[] Gradient(float segment)
+    {
+        int length = this.positions.Count;
+        Vector3[] direction = new Vector3[length];
+
+        for (int i = 0; i < this.length; i++)
+        {
+            Vector3 next = this.positions[(i + 1) % this.length] - this.positions[i];
+            Vector3 previous = this.positions[(i + this.length - 1) % this.length] - this.positions[i];
+            direction[i] = this.lr * (Spring(next, segment) + Spring(previous, segment));
+        }
+
+        return direction;
+    }
+
+    public float Energy(float segment)
+    {
+        float energy = 0.0f;
+
+        for (int i = 0; i < this.length; i++)
+        {
+            Vector3 tangent = this.positions[(i + 1) % length] - this.positions[i];
+            energy += Mathf.Pow(tangent.magnitude - segment, 2);
+        }
+
+        return energy;
+    }
+
+    private Vector3 Spring(Vector3 v, float segment)
+    {
+        return (v.magnitude - segment) * v / v.magnitude;
+    }
+}
