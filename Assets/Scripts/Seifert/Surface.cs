5e3ad650b326a535a2679e7b951b159233a7d064
1123sodium <1997081506124772930na1123@gmail.com>
2020-10-10 15:15:25 +0900

    Add Surface

---
+++ Assets/Scripts/Seifert/Surface.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surface
+{
+    private List<Vector3> boundary;
+    private List<Vector3> vertices;
+    private List<int> triangles;
+
+    public Surface(List<Vector3> boundary)
+    {
+        this.boundary = boundary;
+        this.Initialize();
+    }
+
+    private void Initialize()
+    {
+
+    }
+
+    private void GetMinimal()
+    {
+
+    }
+
+    private void AreaMinimizing()
+    {
+
+    }
+
+    private void LaplacianFairing()
+    {
+
+    }
+
+    private void EdgeSwapping()
+    {
+
+    }
+}
